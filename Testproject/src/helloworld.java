b30906ed8928eaef6a62efe9c731182e205adf3e
sachi <sachi@DESKTOP-9C71R00>
2022-02-23 11:43:20 +1100

    Tommorrow is going to be great

--- Testproject/src/helloworld.java
+++ Testproject/src/helloworld.java
@@ -3,7 +3,7 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World Tommorow");
+		System.out.println("Hello World Tommorow Is Going to be great");
 
 	}
 
